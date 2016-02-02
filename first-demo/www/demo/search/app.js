a3487b2ff6c1d13410b82f7fa70c7c60d9980cdf
zuifengke <yehui1989@qq.com>
2016-02-02 11:13:46 +0800

    提交代码 1、增加angular.js 2、demo案例

---
+++ first-demo/www/demo/search/app.js
@@ -0,0 +1,23 @@
+﻿var searchApp = angular.module('searchApp', []);
+
+searchApp.factory('ResultsFactory', ['$http', function ($http) {
+    return {
+        getResults: function (query) {
+            return $http.post('/getResults', query);
+        }
+    };
+}]);
+
+searchApp.controller('AppCtrl', ['$scope', 'ResultsFactory', function ($scope, ResultsFactory) {
+    $scope.query = '';
+    $scope.results = [];
+    $scope.search = function () {
+        var q = {
+            query: $scope.query
+        };
+        ResultsFactory.getResults(q)
+        .then(function (response) {
+            $scope.results = response.data.results;
+        });
+    }
+}]);
